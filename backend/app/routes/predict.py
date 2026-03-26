5f42cc397e38c6d4787b24506443294b005d0a97
acer <adigum17@gmail.com>
2026-03-26 16:20:13 +0700

    edit jwt

--- backend/app/routes/predict.py
+++ backend/app/routes/predict.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request
 from app.services.predict_service import predict_product
+from flask_jwt_extended import jwt_required
 
 predict_bp = Blueprint('predict', __name__)
 
